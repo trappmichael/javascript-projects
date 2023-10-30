3df27be4e63f2774b572d7bf30c4b257e0191d5a
trappmichael <142745435+trappmichael@users.noreply.github.com>
2023-10-30 18:31:55 -0500

    Completed Chapter 23 Exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,26 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", function(event) {
+        paragraph.innerHTML = "Houston! We have liftoff!";
+
+        missionAbort.addEventListener("click", function(event) {
+            let abortMissionConfirm = window.confirm("Are you sure you want to abort the mission?");
+            if (abortMissionConfirm = true) {
+                paragraph.innerHTML = "Mission aborted! Space shuttle returning home.";
+            };
+        })
+
+    });
+
+    missionAbort.addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", function(event) {
+        event.target.style.backgroundColor = '';
+    });
+
 }
 
 window.addEventListener("load", init);
