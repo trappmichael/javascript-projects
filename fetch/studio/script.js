dc0812ca5a6c5c22de9930516677d7475ebfb8f6
trappmichael <142745435+trappmichael@users.noreply.github.com>
2023-11-09 15:45:45 -0600

    Class 16 Studio Complete

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -4,32 +4,32 @@ window.addEventListener("load", function() {
         response.json().then(function(json) {
             const divAstronauts = document.getElementById("container");
             
-            function findMinHoursInSpace(astronautArray) {
-                let minAstronaut = astronautArray[0];
-                for (i = 0; i < astronautArray.length; i++){
-                  if (astronautArray[i].hoursInSpace < minAstronaut.hoursInSpace){
-                    minAstronaut = astronautArray[i];
+            function findObjectWithMinValue(objectArray, keyToSearch) {
+                let objectWithMinValue = objectArray[0];
+                for (let i = 0; i < objectArray.length; i++){
+                  if (objectArray[i][keyToSearch] < objectWithMinValue[keyToSearch]){
+                    objectWithMinValue = objectArray[i];
                   };
                 };
-                return minAstronaut;
+                return objectWithMinValue;
             };
 
-            function sortJson(astronautArray) {
-                let sortedAstronautArray = [];
-                while (astronautArray.length > 0) {
-                    sortedAstronautArray.push(findMinHoursInSpace(astronautArray));
-                    astronautArray.splice(astronautArray.indexOf(findMinHoursInSpace(astronautArray)),1);
+            function sortObjArrayLowToHigh(objectArray, keyToSearch) {
+                let sortedJson = [];
+                while (objectArray.length > 0) {
+                    sortedJson.push(findObjectWithMinValue(objectArray, keyToSearch));
+                    objectArray.splice(objectArray.indexOf(findObjectWithMinValue(objectArray, keyToSearch)),1);
                 }
-                return sortedAstronautArray;
+                return sortedJson;
             };
 
-            let jsonOrdered = sortJson(Json);
+            let jsonOrdered = sortObjArrayLowToHigh(json, "hoursInSpace");
 
             for (let i = 0; i < jsonOrdered.length; i++) {
                 divAstronauts.innerHTML += `
                     <div class="astronaut">
                         <div class="bio">
-                            <h3>${json[i].firstName} ${jsonOrdered[i].lastName}</h3>
+                            <h3>${jsonOrdered[i].firstName} ${jsonOrdered[i].lastName}</h3>
                             <ul>
                                 <li>Hours in space: ${jsonOrdered[i].hoursInSpace}</li>
                                 <li>Active: ${jsonOrdered[i].active}</li>
